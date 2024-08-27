2cf5bcf1dec4bfc95c11f752d10eea7d24be99c2
Fardus-Hassan <webdev.fardus@gmail.com>
2024-08-28 02:38:46 +0600

    update resume

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import LandingPage from './LandingPage/LandingPage.jsx'
 import WithLoading from './Components/WithLoading.jsx'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <WithLoading> <LandingPage></LandingPage></WithLoading>
